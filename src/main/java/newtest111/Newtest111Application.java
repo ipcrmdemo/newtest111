844b7b7881b4f5e2ca2284d436ee7c83c0c323dd
Matt Cadorette <mcadorette@gmail.com>
2018-10-16 07:18:52 -0400

    Initial commit from Atomist

---
+++ src/main/java/newtest111/Newtest111Application.java
@@ -0,0 +1,12 @@
+package newtest111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newtest111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newtest111Application.class, args);
+	}
+}
